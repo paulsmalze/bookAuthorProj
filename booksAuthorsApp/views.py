ee7c98d129ae2f5233584edee85f59e70e5b7e86
paulsmalze <waffwegs@yahoo.com>
2021-05-16 17:17:26 -0500

    added templates,static,urlspatterns and views

--- booksAuthorsApp/views.py
+++ booksAuthorsApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import*
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'books.html',{"books": Book.objects.all()})
+
+def authors(request):
+    return render(request, 'authors.html',{"authors":Author.objects.all()})
+
+def create_book(request):
+    Book.objects.create(title=request.POST['title'],description=request.POST['description'])
+    return redirect('/books')
+
+def create_author(request):
+    Book.objects.create(title=request.POST['first_name'],description=request.POST['last_name'],notes=request.POST['notes'])
+    return redirect('/authors')
